2632203c57e5d5286a59e2891d89b4885a2f1a28
Mansi Barodia <mansi.ece06@gmail.com>
2018-04-18 07:08:23 -0500

    Updating main activity's source file and .xml file.

--- app/src/main/java/com/mario/mansibarodia/batteryhealth/MainActivity.java
+++ app/src/main/java/com/mario/mansibarodia/batteryhealth/MainActivity.java
@@ -12,6 +12,7 @@ import android.widget.TextView;
 public class MainActivity extends AppCompatActivity {
 
     private static TextView batteryHealthTextView;
+    private static TextView batteryPercentageTextView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,6 +20,7 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         batteryHealthTextView = findViewById(R.id.battery_health);
+        batteryPercentageTextView = findViewById(R.id.battery_percent);
         getApplicationContext().registerReceiver(batteryHealthReceiver, new IntentFilter(Intent.ACTION_BATTERY_CHANGED));
     }
 
@@ -30,8 +32,6 @@ public class MainActivity extends AppCompatActivity {
             int health = intent.getIntExtra(BatteryManager.EXTRA_HEALTH, 0);
             int batteryPercentage = intent.getIntExtra(BatteryManager.EXTRA_LEVEL, 0);
             int plugged = intent.getIntExtra(BatteryManager.EXTRA_PLUGGED, -1);
-            int present = intent.getIntExtra(BatteryManager.EXTRA_PRESENT, 0);
-            int scale = intent.getIntExtra(BatteryManager.EXTRA_SCALE, 0);
             int charging_status = intent.getIntExtra(BatteryManager.EXTRA_STATUS, -1);
             technology = intent.getStringExtra(BatteryManager.EXTRA_TECHNOLOGY);
             int temp_celsius = (intent.getIntExtra(BatteryManager.EXTRA_TEMPERATURE, 0)/10);
@@ -41,49 +41,65 @@ public class MainActivity extends AppCompatActivity {
             switch (health) {
                 case BatteryManager.BATTERY_HEALTH_COLD:
                     condition = "Cold";
+                    break;
                 case BatteryManager.BATTERY_HEALTH_DEAD:
                     condition = "Dead";
+                    break;
                 case BatteryManager.BATTERY_HEALTH_GOOD:
                     condition = "Good";
+                    break;
                 case BatteryManager.BATTERY_HEALTH_OVER_VOLTAGE:
                     condition = "Over Voltage";
+                    break;
                 case BatteryManager.BATTERY_HEALTH_OVERHEAT:
                     condition = "Overheat";
+                    break;
                 case BatteryManager.BATTERY_HEALTH_UNKNOWN:
                     condition = "Unknown";
+                    break;
                 case BatteryManager.BATTERY_HEALTH_UNSPECIFIED_FAILURE:
                     condition = "Unspecified Failure";
+                    break;
             }
 
             switch (plugged) {
                 case BatteryManager.BATTERY_PLUGGED_AC:
                     source = "AC Adapter";
+                    break;
                 case BatteryManager.BATTERY_PLUGGED_USB:
                     source = "USB";
+                    break;
                 case BatteryManager.BATTERY_PLUGGED_WIRELESS:
                     source = "Wireless";
+                    break;
             }
 
             switch (charging_status) {
                 case BatteryManager.BATTERY_STATUS_CHARGING:
                     status = "Charging";
+                    break;
                 case BatteryManager.BATTERY_STATUS_DISCHARGING:
                     status = "Discharging";
+                    break;
                 case BatteryManager.BATTERY_STATUS_FULL:
                     status = "Fully charged";
+                    break;
                 case BatteryManager.BATTERY_STATUS_NOT_CHARGING:
                     status = "Not charging";
+                    break;
                 case BatteryManager.BATTERY_STATUS_UNKNOWN:
                     status = "Unknown";
+                    break;
             }
 
-            batteryHealthTextView.setText("Battery Percentage: "+batteryPercentage+"%\n\n"+
-                    "Battery Condition: "+ condition +"\n\n"+
-                    "Battery Temperature: "+ temp_celsius +"C /"+ temp_f+"F\n\n"+
-                    "Power Source: "+ source +"\n\n"+
-                    "Charging Status: " + status +"\n\n"+
-                    "Battery Type: " + technology +"\n\n"+
-                    "Voltage: " + voltage + "mV");
+//            batteryPercentageTextView.setText("Battery Percentage: "+batteryPercentage+"%\n\n");
+//
+//            batteryHealthTextView.setText("Battery Condition: "+ condition +"\n\n"+
+//                    "Phone Fever: "+ temp_celsius +"C / "+ temp_f+"F\n\n"+
+//                    "Oxygen Supplier: "+ source +"\n\n"+
+//                    "Charging Status: " + status +"\n\n"+
+//                    "Battery Type: " + technology +"\n\n"+
+//                    "Voltage: " + voltage + "mV");
         }
     };
 }
